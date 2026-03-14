a7e77ba7f8a8573c7dd05ecca773a9bdb1836b1c
ChinzeIt <chinzeit@yandex.ru>
2026-03-14 19:16:18 +0300

    add font for all

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char **argv) {
     QApplication app(argc, argv);
 
+    QFont font("San Francisco", 21);
+    QApplication::setFont(font);
+
     mainwindow w;
     w.setWindowIcon(QIcon(":/icons/app_icons.png"));
     w.show();
